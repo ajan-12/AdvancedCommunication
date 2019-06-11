3a3ce7e14f589ebfeb7cbb352a1a9aed44c68d98
ajan-12 <36973731+ajan-12@users.noreply.github.com>
2019-06-12 00:21:00 +0300

    Enhanced the message syntax more and fixed EventPriority of PlayerChatEvent.java

--- src/main/java/me/ajan12/advancedcommunication/Listeners/PlayerChatEvent.java
+++ src/main/java/me/ajan12/advancedcommunication/Listeners/PlayerChatEvent.java
@@ -12,8 +12,8 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class PlayerChatEvent implements Listener {
 
-    //Priority is HIGH because we don't want the other plugins to do their jobs first. INCLUDING DISCORDSRV
-    @EventHandler(priority = EventPriority.HIGHEST)
+    //Priority is MONITOR because we don't want the other plugins to do their jobs first. ESPECIALLY DISCORDSRV
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onChat(AsyncPlayerChatEvent e) {
 
         //Checking if any other plugin cancels this.
@@ -33,12 +33,12 @@ public class PlayerChatEvent implements Listener {
 
         //Sending the message to both target player and sender player
         target.sendMessage(
-                ChatColor.GOLD + "[ " + ChatColor.AQUA + e.getPlayer().getDisplayName() +
-                ChatColor.GOLD + " ] >> [ " + ChatColor.RED + "You" + ChatColor.GOLD + " ] » " +
+                ChatColor.GOLD + "[" + ChatColor.AQUA + e.getPlayer().getDisplayName() +
+                ChatColor.GOLD + "] > [" + ChatColor.RED + "You" + ChatColor.GOLD + "] » " +
                 ChatColor.RESET + e.getMessage());
         e.getPlayer().sendMessage(
-                ChatColor.GOLD + "[ " + ChatColor.RED + "You" +
-                ChatColor.GOLD + " ] >> [ " + ChatColor.AQUA + target.getDisplayName() + ChatColor.GOLD + " ] » " +
+                ChatColor.GOLD + "[" + ChatColor.RED + "You" +
+                ChatColor.GOLD + "] > [" + ChatColor.AQUA + target.getDisplayName() + ChatColor.GOLD + "] » " +
                 ChatColor.RESET + e.getMessage());
 
         //Cancelling the event because we don't want to send multiple messages on chat.
